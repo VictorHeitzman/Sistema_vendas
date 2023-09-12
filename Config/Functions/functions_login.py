81e173a5692bcac61f3da3fbb317cd0588df8569
heitz <heitzmam@gmail.com>
2023-09-12 19:24:05 -0300

    dev

---
+++ Config/Functions/functions_login.py
@@ -0,0 +1,56 @@
+from Config.Modulos.modulos import *
+from Config.Functions.functions_db import DB
+
+from tela_cadastro_usuario import Screen_cadastre_user
+from tela_menu import Screen_menu
+
+
+class Functions(DB):
+
+    def login(self):
+        self.conect_db()
+
+        self.user = self.input_login.get()
+        self.password = self.input_senha.get()
+
+        self.cursor.execute('SELECT * FROM usuarios\
+                            WHERE nome = ? AND senha = ?',(self.user, self.password))
+        
+        login = self.cursor.fetchall()
+        if login:
+            print(login)
+
+            self.root_login.destroy()
+            Screen_menu()
+
+        else:
+            print('user invalid')
+            txt_login = Label(text='user invalid',
+                            font=self.font,
+                            background=self.background,
+                            fg='red')
+            txt_login.place(relheight=0.10,relwidth=0.30,relx=0.38,rely=0.55)
+        
+        self.desconect_db()
+
+    
+    def screen_cadastre_user(self):
+
+        self.user = self.input_login.get()
+        self.password = self.input_senha.get()
+
+        self.admin = 'admin'
+        self.senha_admin = 'admin'
+
+        if self.user == 'admin' and self.password == 'admin':
+        
+            Screen_cadastre_user()
+
+        else:
+            print('user invalid')
+            txt_login = Label(text='user invalid',
+                            font=self.font,
+                            background=self.background,
+                            fg='red')
+            txt_login.place(relheight=0.10,relwidth=0.30,relx=0.38,rely=0.55)
+        
