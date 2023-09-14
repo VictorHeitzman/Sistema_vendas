dcaf59f3ec972555e2788ef4c1ecc153652f98c1
heitz <heitzmam@gmail.com>
2023-09-14 18:07:34 -0300

    dev

--- output/main/Config/Modulos/modulos.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-import sqlite3
-from tkinter import messagebox
-from datetime import date as date
-from tkcalendar import Calendar, DateEntry  
-import pandas as pd
-from sqlite3 import Error as erro_sql
